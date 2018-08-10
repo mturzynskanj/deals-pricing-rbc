e88fdcaf80c850f89eac3d0afc64ab1282b1cc51
Maria Turzynska <mturzynskanj@gmail.com>
2018-08-10 06:55:34 -0400

    Added addition nested rest examples

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ import store from './store'
 
     const addToArray = (arr) => (item) => arr.concat(item);
     const removeFromArray = (arr) => (item) => {
-        let idx= arr.indexOf(item);
-        let newArray = [...arr.slice(0, idx),...arr.slice(idx+1)]
+        let idx = arr.indexOf(item);
+        let newArray = [...arr.slice(0, idx), ...arr.slice(idx + 1)]
         return newArray;
     }
 
@@ -34,28 +34,111 @@ import store from './store'
     };
 
     let newMaria = {
-        age:1000
+        age: 1000
     }
 
     let arrayOfObjects = [{
-        name:'maria', age:22
-    },{
-        name: 'karol', age:44
-    },{
-        name: 'blazje',age:11
-    },{
-        name:'helena', age:55
+        name: 'maria', age: 22
+    }, {
+        name: 'karol', age: 44
+    }, {
+        name: 'blazje', age: 11
+    }, {
+        name: 'helena', age: 55
     }]
 
-    let test = arrayOfObjects.map(obj=>{
-        if(obj.name ==='maria'){
-            return {...obj, ...newMaria}
+    let test = arrayOfObjects.map(obj => {
+        if (obj.name === 'maria') {
+            return { ...obj, ...newMaria }
         } else {
-            return  obj;
+            return obj;
         }
     })
 
     console.log('test is', test);
+
+    let projects = {
+        items: {
+            1: {
+                name: 'project1',
+                tasks: [1, 2]
+
+            },
+            2: {
+                name: 'project2',
+                tasks: [3, 4]
+            }
+        },
+        tasks: {
+            items: {
+                1: {
+                    name: 'learn react',
+                    date: 'march 2019'
+                },
+                2: {
+                    name: 'learn redux',
+                    date: 'may 2019'
+                }
+
+            }
+        }
+
+    };
+
+    let requirement = {
+        id: 1,
+        deadline: 'July'
+    }
+
+    const { id, ...projectInfo } = requirement;
+
+    let newProject = {
+        ...projects,
+        items: {
+            ...projects.items,
+            1: { ...projects.items[1], status: 'finished' }
+        }
+    };
+
+    console.log('newProject', newProject);
+
+    let newProject2 = {
+        ...projects,
+        items: {
+            ...projects.items,
+            2: { ...projects.items[2], ...projectInfo }
+        }
+
+
+
+    }
+
+    console.log('project 2', newProject2);
+
+
+    let newTasks = {
+        ...projects.tasks,
+        items: {
+            ...projects.tasks.items,
+            1: { ...projects.tasks.items[1], 'friend': 'dupa' },
+            2: { ...projects.tasks.items[2], 'test': [].concat('test') }
+        }
+    }
+
+    let newTasks2 = {
+        ...projects.tasks,
+        items: {
+            ...projects.tasks.items,
+            2: {
+               ...projects.tasks.items[2],'lname':'woj', 
+               'test': [].concat('dupa')
+            }
+        }
+    }
+
+    console.log('new Task', newTasks2);
+
+
 }
 
 ReactDOM.render(
